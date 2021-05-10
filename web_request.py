348ff0b4b85fb0581817a19f7e1c2e75b50e9dd9
korshunovamm <korshunova.mm@phystech.edu>
2021-05-10 03:01:42 +0300

    upd

--- web_request.py
+++ web_request.py
@@ -1,12 +1,26 @@
 import requests
 from bs4 import BeautifulSoup
-import time
+from itertools import cycle
 
 
 class Parser:
     def __init__(self, url):
         self.url = url
-        response = requests.get(self.url)
+        headers = [
+            {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.8; rv:45.0) Gecko/20100101 Firefox/45.0'},
+            {
+                'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.111 YaBrowser/21.2.1.94 (beta) Yowser/2.5 Safari/537.36'},
+            {
+                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.146 Safari/537.36'},
+            {
+                'User-Agent': 'Mozilla/5.0 (Windows NT 6.3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.104 Safari/537.36'},
+            {
+                'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.141 Safari/537.36'},
+            {
+                'User_Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.141 Safari/537.36'}
+        ]
+
+        response = requests.get(self.url, headers=headers[3])
         response.encoding = "utf-8"
         self.html = response.text
         self.soup = BeautifulSoup(self.html, "lxml")
@@ -23,21 +37,62 @@ class Parser:
                 weather_week[date.text] = day["summary"]
         return weather_week
 
-    def find_weather_day(self, number_day, number_daytime):
-        weather_day = []
-        number_day = f"div:nth-of-type({number_day})"
-        describe_day = self.soup.select(number_day, class_="row collapse align-middle")
-        print(describe_day)
-        # for item in describe_day:
-        #     if describe_day:
-        #         describe_date = describe_day.find_all_previous("span", class_="show-for-medium")
-        # describe_day = self.soup.select("div", class_="row collapse align-middle")
-        # weather_day.append(describe_dat)
-        return weather_day
+    def find_weather_day(self):
+        weather_day_date = []
+        weather_day_temperature = []
+        weather_day_feeled_temperature = []
+        weather_day_wind = []
+
+        describe_day = self.soup.find_all(["span"], {"class": ["show-for-medium"]})
+        for item in describe_day:
+            if item:
+                weather_day_date.append(item.text)
+                #print(item.text)
+
+        describe_temperature = self.soup.find_all(["tr"], {"class": ["temperature"]})
+        for item in describe_temperature:
+            list_temperature = item.text.replace("\n", '').replace("  ", '').split('\t')
+            list_temperature_describe = ["Температура воздуха ночью: ", "Температура воздуха утром: ",
+                                         "Температура воздуха днем: ", "Температура воздуха вечером: "]
+            list_day_temperature = [list_temperature[i]
+                                    for i in range(len(list_temperature))
+                                    if list_temperature[i] != '' and i % 4 == 0 and i]
+            weather_day_temperature.append(
+                [list_temperature_describe[i] + list_day_temperature[i] for i in range(4)])
+
+        describe_feeled_temperature = self.soup.find_all(["tr"], {"class": ["feeled-temperature"]})
+        for item in describe_feeled_temperature:
+            list_feeled_temperature = item.text.replace("\n", '').replace("  ", '').replace("Ощущается", "").split("°")
+            list_feeled_temperature_describe = ["Ощущается ночью: ", "Ощущается утром: ",
+                                                "Ощущается днем: ", "Ощущается вечером: "]
+            if len(list_feeled_temperature) != 1:
+                list_day_feeled_temperature = [list_feeled_temperature[i] + "°"
+                                               for i in range(len(list_feeled_temperature))
+                                               if list_feeled_temperature[i] != '' and i % 2 != 0]
+                weather_day_feeled_temperature.append(
+                    [list_feeled_temperature_describe[i] + list_day_feeled_temperature[i] for i in range(4)])
+
+        describe_wind = self.soup.find_all(["tr"], {"class": ["wind"]}, {"title": [True]})
+        for item in describe_wind:
+            list_wind = item.text.replace("  ", '').replace("\n", '').replace("Ветер", '').split("м/с")
+            list_wind_describe = ["Ветер ночью: ", "Ветер утром: ",
+                                  "Ветер днем: ", "Ветер вечером: "]
+            list_day_wind = [list_wind[i]
+                             for i in range(len(list_wind))
+                             if list_wind[i] != '' and i % 2 != 0]
+            weather_day_wind.append([list_wind_describe[i] + list_day_wind[i] for i in range(4)])
+
+        # print(weather_day_date)
+        # print(weather_day_temperature)
+        # print(weather_day_feeled_temperature)
+        # print(weather_day_wind)
+        return [weather_day_date, weather_day_temperature, weather_day_feeled_temperature, weather_day_wind]
 
 
-# t1 = time.time()
-# t2 = time.time()
-# print("Time of execution: ", t2 - t1)
 # print(Parser('https://www.meteoservice.ru/weather/week/moskva').find_weather_week())
-# print(Parser('https://www.meteoservice.ru/weather/week/moskva').find_weather_day(1, 2))
+#Parser('https://www.meteoservice.ru/weather/week/moskva').find_weather_day()
+
+try:
+    Parser('https://www.meteoservice.ru/weather/week/moskva').find_weather_day()
+except Exception as e:
+    print("Error occured: ", e)
